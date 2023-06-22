0140b248926625b3d7016556954f10fc48732e08
Alexandru Stoicea <137204266+alexandrustoicea@users.noreply.github.com>
2023-06-22 18:50:48 +0300

    first exercise

---
+++ Curs 1 exercitiu/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Declare Variables
+Declare five variables choosing for each of them the most appropriate of the types byte, sbyte, short, ushort, int, uint, long, ulong to represent the following values: 52130, -115, 
+4825932, 97, -10000.Choose a large enough type for each number to ensure it will fit in it. Try to compile the code.
+*/
+
+ushort val = 52130;
+
+sbyte val1 = -115;
+
+int val2 = 4825932;
+
+sbyte val3 = 97;
+
+short val4 = -10000;
+
+Console.WriteLine("Valoarea 1: " + val  + ", valoarea 2: " + val1 + ", valoarea 3: " + val2 + ", valoarea 4: " + val3 + ", valoarea 5: " + val4 + " sau mai avem varianta de mai jos:");
+
+Console.WriteLine($"{val}, {val1}, {val2}, {val3}, {val4}");
+
+Console.WriteLine("Hello, World!");
